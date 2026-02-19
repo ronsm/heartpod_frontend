8760b5ca79512df5eacdf4029e615322efe572e0
Ronnie Smith <ronniesmith@outlook.com>
2026-02-19 20:16:12 +0000

    simplified android app

--- app/src/main/java/org/hwu/care/healthub/data/PiApi.kt
+++
@@ -1,9 +0,0 @@
-package org.hwu.care.healthub.data
-
-import org.hwu.care.healthub.data.models.Reading
-
-interface PiApi {
-    suspend fun getLatestReading(deviceId: String): Reading?
-    suspend fun setFocus(deviceId: String)
-    suspend fun getItemState(itemName: String): String
-}
